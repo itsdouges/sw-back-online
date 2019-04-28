64f5cc5f962b669f3150fe81ac3dad28778e35cf
madou <laheen@gmail.com>
2019-04-28 23:06:21 +1000

    finish proof of concept

--- test/app.js
+++ test/app.js
@@ -1,11 +1,68 @@
-import * as sw from '../dist/index.js';
+// Scope important to limit it to a particular sub-directory.
+navigator.serviceWorker.register('./sw.js', { scope: '/test/' });
 
-console.log(sw);
-
-const form = document.getElementById('form');
-
-form.addEventListener('submit', (event) => {
+document.getElementById('form').addEventListener('submit', async (event) => {
   event.preventDefault();
-  const data = document.getElementById('name').value;
-  sw.post({ data });
+
+  try {
+    // send form data, yo.
+    // navigator.serviceWorker.controller.postMessage({
+    //   sync: true,
+    // });
+    await fetch('https://postman-echo.com/post', {
+      method: 'POST',
+      body: JSON.stringify({ test: 'data' }),
+      mode: 'no-cors',
+    });
+    console.log('success posting.');
+  } catch (e) {
+    console.log('network failure, setting up bg sync');
+    const swRegistration = await navigator.serviceWorker.ready;
+    await swRegistration.sync.register('fetchPost');
+  }
 });
+
+// https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+document.getElementById('subscribe').addEventListener('click', async () => {
+  const permission = await Notification.requestPermission();
+  console.log(permission);
+});
+
+document
+  .getElementById('test-notification')
+  .addEventListener('click', async () => {
+    const registration = await navigator.serviceWorker.ready;
+    if (Notification.permission === 'denied') {
+      alert('Yo gimmie access dawg');
+    } else {
+      registration.showNotification('Request successfully submitted', {
+        body: 'Click here to view your request.',
+        // Action only on android, probably.
+        // https://developer.mozilla.org/en-US/docs/Web/API/NotificationEvent/action
+      });
+    }
+  });
+
+navigator.serviceWorker.addEventListener('message', async function(event) {
+  console.log('sw talk to me', event);
+
+  if (event.data.showNotification) {
+    const registration = await navigator.serviceWorker.ready;
+    if (Notification.permission === 'denied') {
+      alert('Yo gimmie access dawg');
+    } else {
+      registration.showNotification(
+        event.data.showNotification.name,
+        event.data.showNotification.options,
+      );
+    }
+  }
+});
+
+// window.addEventListener('offline', function() {
+//   alert('You have lost internet access!');
+// });
+
+// window.addEventListener('online', function() {
+//   alert('You are back online');
+// });
---
+++ test/sw.js
@@ -0,0 +1,59 @@
+self.addEventListener(
+  'notificationclick',
+  (event) => {
+    event.notification.close();
+    if (!event.action) {
+      clients.openWindow('/test/#MAD-123');
+    }
+  },
+  false,
+);
+
+// https://developers.google.com/web/updates/2015/12/background-sync
+// If this doesn't work reopen chrome.
+// If that still doesn't work unregister the sw.
+self.addEventListener('sync', function(event) {
+  console.log('hell? :(');
+
+  if (event.tag === 'fetchPost') {
+    console.log('hmm found sync', event);
+
+    const promise = fetch('https://postman-echo.com/post', {
+      method: 'POST',
+      body: JSON.stringify({ test: 'data' }),
+      mode: 'no-cors',
+    })
+      .then(() => {
+        console.log('success!');
+
+        return self.clients.matchAll();
+      })
+      .then((clients) => {
+        clients.forEach((client) => {
+          client.postMessage({
+            showNotification: {
+              name: 'Request successfully submitted',
+              options: {
+                body: 'Click here to view your request.',
+              },
+            },
+          });
+        });
+      });
+
+    event.waitUntil(promise);
+  }
+});
+
+// https://www.twilio.com/blog/2017/02/send-messages-when-youre-back-online-with-service-workers-and-background-sync.html
+// https://blog.formpl.us/how-to-handle-post-put-requests-in-offline-applications-using-service-workers-indexedb-and-da7d0798a9ab
+// self.addEventListener('fetch', function(event) {
+//   console.log('someone trying to fetch');
+//   console.log(event);
+// });
+self.addEventListener('message', function(event) {
+  if (event.data.sync) {
+    console.log('saving form data', event.data);
+  }
+});
+// end
